78f67c4fa81815630ac53ac4dcc069853ce848a6
yuanpeirong <yuanpeirong@vip.qq.com>
2022-04-08 21:50:32 +0800

    2022-04-07

---
+++ airtac/Javascript/LSH.js
@@ -0,0 +1,69 @@
+function MyShowIMG1() 
+{
+	switch(document.getElementById("ID_selectSYZT").value)
+	{
+		case "SYZT_1":
+			switch(document.getElementById("ID_selectDGSandHKS").value)
+			{
+				case "DGSandHKS_11":
+					document.getElementById("ID_MyShowIMG1").src="images/1-1-1.jpg";break;
+				case "DGSandHKS_12":
+					document.getElementById("ID_MyShowIMG1").src="images/1-1-2.jpg";break;
+				case "DGSandHKS_13":
+					document.getElementById("ID_MyShowIMG1").src="images/1-1-3.jpg";break;
+				case "DGSandHKS_14":
+					document.getElementById("ID_MyShowIMG1").src="images/1-1-4.jpg";break;
+				case "DGSandHKS_21":
+					document.getElementById("ID_MyShowIMG1").src="images/1-2-1.jpg";break;
+				case "DGSandHKS_22":
+					document.getElementById("ID_MyShowIMG1").src="images/1-2-2.jpg";break;
+				case "DGSandHKS_23":
+					document.getElementById("ID_MyShowIMG1").src="images/1-2-3.jpg";break;
+				case "DGSandHKS_24":
+					document.getElementById("ID_MyShowIMG1").src="images/1-2-4.jpg";break;
+			}
+			break;
+		case "SYZT_2":
+			switch(document.getElementById("ID_selectDGSandHKS").value)
+			{
+				case "DGSandHKS_11":
+					document.getElementById("ID_MyShowIMG1").src="images/2-1-1.jpg";break;
+				case "DGSandHKS_12":
+					document.getElementById("ID_MyShowIMG1").src="images/2-1-2.jpg";break;
+				case "DGSandHKS_13":
+					document.getElementById("ID_MyShowIMG1").src="images/2-1-3.jpg";break;
+				case "DGSandHKS_14":
+					document.getElementById("ID_MyShowIMG1").src="images/2-1-4.jpg";break;
+				case "DGSandHKS_21":
+					document.getElementById("ID_MyShowIMG1").src="images/2-2-1.jpg";break;
+				case "DGSandHKS_22":
+					document.getElementById("ID_MyShowIMG1").src="images/2-2-2.jpg";break;
+				case "DGSandHKS_23":
+					document.getElementById("ID_MyShowIMG1").src="images/2-2-3.jpg";break;
+				case "DGSandHKS_24":
+					document.getElementById("ID_MyShowIMG1").src="images/2-2-4.jpg";break;
+			}
+			break;
+		case "SYZT_3":
+			switch(document.getElementById("ID_selectDGSandHKS").value)
+			{
+				case "DGSandHKS_11":
+					document.getElementById("ID_MyShowIMG1").src="images/3-1-1.jpg";break;
+				case "DGSandHKS_12":
+					document.getElementById("ID_MyShowIMG1").src="images/3-1-2.jpg";break;
+				case "DGSandHKS_13":
+					document.getElementById("ID_MyShowIMG1").src="images/3-1-3.jpg";break;
+				case "DGSandHKS_14":
+					document.getElementById("ID_MyShowIMG1").src="images/3-1-4.jpg";break;
+				case "DGSandHKS_21":
+					document.getElementById("ID_MyShowIMG1").src="images/3-2-1.jpg";break;
+				case "DGSandHKS_22":
+					document.getElementById("ID_MyShowIMG1").src="images/3-2-2.jpg";break;
+				case "DGSandHKS_23":
+					document.getElementById("ID_MyShowIMG1").src="images/3-2-3.jpg";break;
+				case "DGSandHKS_24":
+					document.getElementById("ID_MyShowIMG1").src="images/3-2-4.jpg";break;
+			}
+			break;
+	}
+}
